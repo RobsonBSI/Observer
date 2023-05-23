a10780137107d796c76b09488f66a206109252cc
RobsonBSI <103039603+RobsonBSI@users.noreply.github.com>
2023-05-23 14:47:54 -0300

    teste

--- src/main/java/Pedido.java
+++ src/main/java/Pedido.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class Pedido {
+import java.util.Observable;
+
+public class Pedido  extends Observable {
+    private Integer numeroPedido;
+    private String nomeLanche;
+    private float valor;
+
+    public Pedido(Integer numero, String nomeLanche, float valor) {
+        this.numeroPedido = numero;
+        this.nomeLanche = nomeLanche;
+        this.valor = valor;
+    }
+
+    public void lancarPedido() {
+        setChanged();
+        notifyObservers();
+    }
+
+    @Override
+    public String toString() {
+        return "Pedido" +
+                "(" + nomeLanche +
+                ",Valor= " + valor +
+                ")";
+    }
 }
--- src/test/java/ClienteTest.java
+++ src/test/java/ClienteTest.java
@@ -1,4 +1,14 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class ClienteTest {
-  
+    @Test
+    void deveNotificarUmCliente() {
+        Pedido pedido = new Pedido(001, "X Bacon",20.50f);
+        Cliente cliente = new Cliente("Robson");
+        cliente.RegistraPedido(pedido);
+        pedido.lancarPedido();
+        assertEquals("Robson,O Pedido(X Bacon,Valor= 20.5),Foi Aceito", cliente.getUltimaNotificacao());
+    }
 }
